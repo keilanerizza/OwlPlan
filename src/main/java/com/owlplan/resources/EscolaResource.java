cb307c91d513a6f6a6683d045f8648eebe60045d
Keilane Rizza <keilanerizza@gmail.com>
2018-09-29 15:04:18 -0300

    Testando a primeira classe de dominio - Escola

--- src/main/java/com/owlplan/resources/EscolaResource.java
+++ src/main/java/com/owlplan/resources/EscolaResource.java
@@ -1,16 +1,29 @@
 package com.owlplan.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.owlplan.domain.Escola;
 
 @RestController
 @RequestMapping(value="/escolas")
 public class EscolaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Escola> listar() {
+		
+		Escola esc1 = new Escola(1, "EMEI Profª. Carmelita Vieira dos Santos");
+		Escola esc2 = new Escola(2, "EMEI Zacarias Pereira da Silva");
+		
+		List<Escola> lista = new ArrayList<>();
+		lista.add(esc1);
+		lista.add(esc2);
+		
+		return lista;
 	}
 
 }
